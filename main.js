ac501f46bd20fd908bf4a8cca7c49d522b8a0a6b
Michael Ning <mning95@gmail.com>
2021-07-26 15:52:06 -0700

    Update game state and UI on click

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function main() {
-  console.log("here");
-}
+// Game states
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let currPlayer = "X";
 
-main();
+const clickCell = ({target}) => {
+  const cellIndex = parseInt(target.getAttribute('data-index'));
+
+  // Do nothing if user clicks occupied cell
+  if (gameState[cellIndex] !== "") return;
+
+  // Update UI and game state
+  target.innerHTML = currPlayer;
+  gameState[cellIndex] = currPlayer;
+};
+
+
+
+document.querySelectorAll('.cell').forEach(c => c.addEventListener('click', clickCell));

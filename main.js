c4aaa742ba5a1ff1d9acd57de2145b2d518ad0ff
Michael Ning <mning95@gmail.com>
2021-07-26 16:15:48 -0700

    Display notification on win

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 // Game states
 let gameState = ["", "", "", "", "", "", "", "", ""];
 let currPlayer = "X";
+const winningCombos = [
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6], 
+];
 
 const clickCell = ({target}) => {
   const cellIndex = parseInt(target.getAttribute('data-index'));
@@ -11,6 +21,14 @@ const clickCell = ({target}) => {
   // Update UI and game state
   target.innerHTML = currPlayer;
   gameState[cellIndex] = currPlayer;
+
+  // Check game status
+  for (const combo of winningCombos) {
+    if (gameState[combo[0]] !== "" && gameState[combo[0]] === gameState[combo[1]] && gameState[combo[1]] === gameState[combo[2]]) {
+      window.alert(`${currPlayer} has won the game!`)
+      return;
+    }
+  }
 };
 
 

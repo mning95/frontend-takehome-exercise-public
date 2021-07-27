dc97265b078a79166321c9cc39c905e70c99c2e1
Michael Ning <mning95@gmail.com>
2021-07-27 00:11:26 -0700

    Replace `"` with `'` for consistency

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let gameState = ["", "", "", "", "", "", "", "", ""];
-let currPlayer = "X";
+let gameState = ['', '', '', '', '', '', '', '', ''];
+let currPlayer = 'X';
 let xWinCount = 0;
 let oWinCount = 0;
 let tieCount = 0;
@@ -29,15 +29,15 @@ const calculatePercent = (num, denom, digits) => ((num / denom) * 100).toFixed(d
 /**
  * Helper function that updates counts, UI, and removes event listeners after game has ended
  * 
- * @param {string} result: "win" or "tie" result
+ * @param {string} result: 'win' or 'tie' result
  */
 const endGame = (result) => {  
   // 1) Update counts
   gameCount++;
   
-  if (result === "win") {
+  if (result === 'win') {
     // Update win count
-    if (currPlayer === "X") {
+    if (currPlayer === 'X') {
       xWinCount++;
     } else {
       oWinCount++;
@@ -49,15 +49,15 @@ const endGame = (result) => {
 
   // 2) Update UI
   // Update status
-  document.querySelector('.status').innerHTML = result === "win" ? `The winner is ${currPlayer}` : 'Tie Game';
+  document.querySelector('.status').innerHTML = result === 'win' ? `The winner is ${currPlayer}` : 'Tie Game';
   // Update scoreboard
   document.querySelector('.win-count-x').innerHTML = `${xWinCount} (${calculatePercent(xWinCount, gameCount, 2)}%)`;
   document.querySelector('.win-count-o').innerHTML = `${oWinCount} (${calculatePercent(oWinCount, gameCount, 2)}%)`;
   document.querySelector('.tie-count').innerHTML = `${tieCount} (${calculatePercent(tieCount, gameCount, 2)}%)`;
   // Switch cursor style to default
-  document.querySelectorAll('.cell').forEach(c => c.style.cursor = "default");
+  document.querySelectorAll('.cell').forEach(c => c.style.cursor = 'default');
   // Display 'play again' button
-  document.querySelector('.play-again').style.visibility = "visible";
+  document.querySelector('.play-again').style.visibility = 'visible';
 
   // 3) Remove event listeners
   document.querySelectorAll('.cell').forEach(c => {
@@ -79,16 +79,16 @@ const checkGameStatus = () => {
     const second = combo[1];
     const third = combo[2];
 
-    // Win if winning combo cells are occupied by same symbol that's not ""
-    if (gameState[first] !== "" && gameState[first] === gameState[second] && gameState[second] === gameState[third]) {
-      endGame("win")
+    // Win if winning combo cells are occupied by same symbol that's not ''
+    if (gameState[first] !== '' && gameState[first] === gameState[second] && gameState[second] === gameState[third]) {
+      endGame('win')
       return true;
     }
   }
 
   // Tie if there are no winning combos and all cells are occupied
-  if (!gameState.includes("")) {
-    endGame("tie");
+  if (!gameState.includes('')) {
+    endGame('tie');
     return true;
   }
 
@@ -105,7 +105,7 @@ const checkGameStatus = () => {
 const placeMove = ({target}) => {
   // Do nothing if user clicks occupied cell
   const cellIndex = parseInt(target.getAttribute('data-index'));
-  if (gameState[cellIndex] !== "") return;
+  if (gameState[cellIndex] !== '') return;
 
   // Update game state and UI
   gameState[cellIndex] = currPlayer;
@@ -117,7 +117,7 @@ const placeMove = ({target}) => {
   if (gameOver) return;
 
   // Switch players and update status UI
-  currPlayer = currPlayer === "X" ? "O" : "X";
+  currPlayer = currPlayer === 'X' ? 'O' : 'X';
   document.querySelector('.status').innerHTML = `${currPlayer}'s Turn`
 
   // Remove mouse hover event listeners to prevent overriding updates to cell made by click event
@@ -151,14 +151,14 @@ const disablePreview = ({target}) => {
 // updates UI, and removes listeners.
 const playAgain = () => {
   // Reset game state and player. Update UI
-  gameState = ["", "", "", "", "", "", "", "", ""];
-  currPlayer = "X";
-  document.querySelectorAll('.cell').forEach(c => c.innerHTML = "");
+  gameState = ['', '', '', '', '', '', '', '', ''];
+  currPlayer = 'X';
+  document.querySelectorAll('.cell').forEach(c => c.innerHTML = '');
   document.querySelector('.status').innerHTML = `${currPlayer}'s Turn`
   // Switch cursor style to pointer
-  document.querySelectorAll('.cell').forEach(c => c.style.cursor = "pointer");
+  document.querySelectorAll('.cell').forEach(c => c.style.cursor = 'pointer');
   // Hide 'play again' button
-  document.querySelector('.play-again').style.visibility = "hidden";
+  document.querySelector('.play-again').style.visibility = 'hidden';
 
   // Re-enable event listeners
   document.querySelectorAll('.cell').forEach(c => {

2540040e70460edd47faebf0bf551f57b461a613
Michael Ning <mning95@gmail.com>
2021-07-26 17:00:50 -0700

    Update game status in status element instead of window.alert

--- main.js
+++ main.js
@@ -14,11 +14,12 @@ const winningCombos = [
 
 // Reset UI, game state, and player. Hide 'play again' button
 const replay = () => {
+  document.querySelector('.status').innerHTML = "";
   document.querySelectorAll('.cell').forEach(c => c.innerHTML = "");
   gameState = ["", "", "", "", "", "", "", "", ""];
   currPlayer = "X";
   document.querySelector('.replay').style.visibility = "hidden";
-}
+};
 
 const clickCell = ({target}) => {
   const cellIndex = parseInt(target.getAttribute('data-index'));
@@ -37,7 +38,7 @@ const clickCell = ({target}) => {
     const third = combo[2];
 
     if (gameState[first] !== "" && gameState[first] === gameState[second] && gameState[second] === gameState[third]) {
-      window.alert(`The winner is ${currPlayer}`);
+      document.querySelector('.status').innerHTML = `The winner is ${currPlayer}`;
       // Display 'play again' button
       document.querySelector('.replay').style.visibility="visible";
       
@@ -47,7 +48,7 @@ const clickCell = ({target}) => {
 
   // Draw if no winning combos and all cells are occupied
   if (!gameState.includes("")) {
-    window.alert('Tie Game');
+    document.querySelector('.status').innerHTML = 'Tie Game';
     // Display 'play again' button
     document.querySelector('.replay').style.visibility="visible";
   }
